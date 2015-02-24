213afcb142c8eb18f491b90f0fe50dc799056de8
David <bafadam@gmail.com>
2015-02-23 18:56:42 -0600

    Fix for datetime controls

--- RomeoTracker/Models/Context/ApplicationDbContext.cs
+++ RomeoTracker/Models/Context/ApplicationDbContext.cs
@@ -24,11 +24,6 @@ namespace RomeoTracker.Models
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            //modelBuilder.Entity<IdentityUserLogin>().HasKey<string>(l => l.UserId);
-            //modelBuilder.Entity<IdentityRole>().HasKey<string>(r => r.Id);
-            //modelBuilder.Entity<IdentityUserRole>().HasKey(r => new { r.RoleId, r.UserId });
-        
         }
 
         #region Events 
